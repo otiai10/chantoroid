3af9a4c5cb1f26dd29fe8da84d37c2a41531fc0e
otiai10 <otiai10@gmail.com>
2015-09-24 18:27:53 +0900

    Save histories in SharedPreference

--- app/src/main/java/com/otiai10/chantroid/MainEntranceActivity.java
+++ app/src/main/java/com/otiai10/chantroid/MainEntranceActivity.java
@@ -3,19 +3,30 @@ package com.otiai10.chantroid;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TableRow;
 import android.widget.Toast;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
-public class MainEntranceActivity extends Activity implements View.OnClickListener {
+public class MainEntranceActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    private String SavedServerPrefKey = "SAVED_SERVERS";
+    private String[] savedServerURLs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,17 @@ public class MainEntranceActivity extends Activity implements View.OnClickListen
         // delegate button events
         Button entryButton = (Button)this.findViewById(R.id.EntryButton);
         entryButton.setOnClickListener(this);
+
+        // 保存されてるアドレスを取得
+        ListView savedServerURLsListView = (ListView)this.findViewById(R.id.savedServerURLsList);
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        Set<String> saved = sp.getStringSet(SavedServerPrefKey, new HashSet<String>());
+
+        this.savedServerURLs = saved.toArray(new String[saved.size()]);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, this.savedServerURLs);
+
+        savedServerURLsListView.setAdapter(adapter);
+        savedServerURLsListView.setOnItemClickListener(this);
     }
 
 
@@ -53,24 +75,44 @@ public class MainEntranceActivity extends Activity implements View.OnClickListen
     public void onClick(View v) {
         EditText serverURL = (EditText)this.findViewById(R.id.serverURL);
         String url = serverURL.getText().toString();
+
         if (url.isEmpty() || url.matches("")) {
             return; // abort
         }
 
-        Log.d(this.getLocalClassName(), url + ":" + this.checkInputIsValidURL(url));
-        if (this.checkInputIsValidURL(url)) {
-            Log.d(this.getLocalClassName(), "ここでほぞん");
-            /*
-            Intent intent = new Intent(this, RoomActivity.class);
-            intent.putExtra("url", url);
-            this.startActivity(intent);
-            */
-            return;
+        if (!this.checkInputIsValidURL(url)) {
+            return; // abort
         }
+
+        intentToServerURL(url);
+    }
+
+    /**
+     * めんどくさいのでActivityにOnItemClickListener実装しちゃいます
+     * @param adapter
+     * @param v
+     * @param position
+     * @param a
+     */
+    public void onItemClick(AdapterView<?>adapter, View v, int position, long a) {
+        intentToServerURL(savedServerURLs[position]);
     }
 
     private boolean checkInputIsValidURL(String input) {
         Pattern URL_PATTERN = Patterns.WEB_URL;
         return URL_PATTERN.matcher(input).matches();
+    }
+
+    private void intentToServerURL(String url) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        Set<String> saved = sp.getStringSet(SavedServerPrefKey, new HashSet<String>());
+
+        saved.add(url);
+        sp.edit().putStringSet(SavedServerPrefKey, saved).apply();
+
+        Intent intent = new Intent(this, RoomActivity.class);
+        intent.putExtra("url", url);
+
+        this.startActivity(intent);
     }
 }
